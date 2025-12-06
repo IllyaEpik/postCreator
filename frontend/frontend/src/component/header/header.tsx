b83603dcc8f10794e49b1c6105ce9de539f29943
Illya <121565465+IllyaEpik@users.noreply.github.com>
2025-12-06 08:17:58 +0200

    media

--- frontend/frontend/src/component/header/header.tsx
+++ frontend/frontend/src/component/header/header.tsx
@@ -1,16 +1,29 @@
 import React from "react";
 import {HeaderButton} from "../header.button/header.button";
+import { ReactComponent as Menu } from "../../assets/svg/menu.svg";
+import { useMediaQuery  } from "react-responsive";
 import styles from "./header.module.css";
 export function Header() {
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 950px)' })
     
     return <header className={styles.header}>
         <HeaderButton href="" text = "postCreator"/>
-        <div id={styles.pages}>
-            <HeaderButton href="/posts" text = "posts"/>
-            <HeaderButton href="/createPost" text = "create post"/>
-            <HeaderButton href="/main" text = "main"/>
-            <HeaderButton href="/language" text = "english"/>
-            <img src="/avatar.png" alt="avatar" />
-        </div>
+        
+            
+            {isTabletOrMobile ? 
+            <div id={styles.pages}>
+            <Menu width={50} height={50} className={styles.menu}/>
+                <img src="/avatar.png" alt="avatar" />
+            </div>
+            :
+            <div id={styles.pages}>
+                <HeaderButton href="/posts" text = "posts"/>
+                <HeaderButton href="/createPost" text = "create post"/>
+                <HeaderButton href="/main" text = "main"/>
+                <HeaderButton href="/language" text = "english"/>
+                <img src="/avatar.png" alt="avatar" />
+            </div>
+            }
+            
     </header>
 }
